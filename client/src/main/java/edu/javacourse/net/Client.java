94b3dbc0ff4557ff7931911113e039aa81f00920
Vladimir <kosakvova@gmail.com>
2022-12-21 14:17:30 +0400

    Initial commit

---
+++ client/src/main/java/edu/javacourse/net/Client.java
@@ -0,0 +1,53 @@
+package edu.javacourse.net;
+
+import java.io.*;
+import java.net.Socket;
+import java.time.LocalDateTime;
+
+public class Client {
+    public static void main(String[] args) throws IOException {
+        for (int i = 0 ; i<1000; i++){
+            SimpleClient sc = new SimpleClient(i);
+            sc.start();
+        }
+    }
+}
+
+class  SimpleClient extends Thread{
+
+    private final static String [] COMMAND = {
+            "HELLO", "MORNING", "DAY", "EVENING"
+    };
+
+    private int cmdNumber;
+
+    public SimpleClient(int cmdNumber){
+        this.cmdNumber = cmdNumber;
+    }
+    public void run(){
+        try {
+
+//            System.out.println("started:" + LocalDateTime.now());
+            Socket socket = new Socket("127.0.0.1", 25225);
+            BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+
+            String command = COMMAND[cmdNumber % COMMAND.length];
+
+            String sb = command + " " + "Vladimir";
+
+            bw.write(sb);
+            bw.newLine();
+            bw.flush();
+
+            String answer = br.readLine();
+            System.out.println("Client got string:" + answer);
+
+            br.close();
+            bw.close();
+//            System.out.println("Finished:" + LocalDateTime.now());
+        }catch (IOException ex){
+            ex.printStackTrace(System.out);
+        }
+    }
+}
